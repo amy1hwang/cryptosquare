e38d8f40cbc430ecdf40717949ee7a80ca309696
Adam Baker and Amy Hwang <amy@>
2016-10-27 11:34:46 -0700

    Square root and round up

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 var cryptosquareFunction = function(string) {
   //take out non letter characters.
   string = string.replace(/[^a-zA-Z-]/g, "").toLowerCase();
-  //string = string.replace(/^[a-zA-Z]*$/gi);
-  console.log(string);
   //make an array of the string
-  //figure out # of columns and # of rows based on the # of the letters in the string.
-  //slice(start, column.length)
+  var letters = string.split("");
+  var squareRootOfLetters = Math.ceil(Math.sqrt(letters.length));
+  console.log(squareRootOfLetters);
+  //figure out# of rows based on the # of the letters in the string.
+  var firstRow = letters.slice(0, squareRootOfLetters);
+  console.log(firstRow);
+  // for(var i = 0, i = squareRootOfLetters, i++) {
+  //
+  // }
+  // //slice(start, column.length)
 }
 
 $(document).ready(function() {
