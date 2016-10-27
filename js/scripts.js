9a8be70688dfd575b9d736f0888e25a456b26c77
Adam Baker and Amy Hwang <adam@>
2016-10-27 16:47:02 -0700

    add forloop function to find column

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
+var stringFunction = function(sentence) {
+  sentence = sentence.replace(/[^a-zA-Z-]/g, "").toLowerCase();
+}
+var squareRootOfLetters = function(string) {
+  return Math.ceil(Math.sqrt(string.length));
+}
 var cryptosquareFunction = function(string) {
-  //take out non letter characters.
-  string = string.replace(/[^a-zA-Z-]/g, "").toLowerCase();
-  //make an array of the string
-  var letters = string.split("");
-  var squareRootOfLetters = Math.ceil(Math.sqrt(letters.length));
-  //slice(start, column.length)
-  var row = []
-  var rowArray = []
-  //var position = []
-  for(var i = 0; i < squareRootOfLetters; i++) {
-    row = letters.splice(0, squareRootOfLetters);
-    rowArray.push(row);
-  }
-  for(var r = 0; r <= row.length; r++) {
-    //position = row.shift();
-  }
-  console.log(rowArray);
-  //console.log(position);
+  var string = stringFunction(string)
+  var x = squareRootOfLetters(string)
+  var column = "";
+  for(var j = 0; j < x; j++) {
+    for(var i = 0; i < string.length; i += x) {
+      column += string.charAt(j + i);
+    };
+  };
+  console.log(column);
+  //   rowArray.push(row);
+  // // }
+  // // for(var r = 0; r < row.length; r++) {
+  //   position = row.splice(0, 1);
+  //   positionArray.push(position);
+  // }
+  // console.log(position);
+  // console.log(positionArray);
 }
 
 $(document).ready(function() {
